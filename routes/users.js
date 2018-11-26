132a33ac0bd05a4dee04114c08171bbe093873ac
Kamila Purymska <kamilapurymska@MacBook-Air-Kamila.local>
2018-11-26 11:13:33 +0100

    search button hbs

--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,37 @@ router.get('/', (req, res, next) => {
   User.find()
     .then((result) => {
       res.render('users/list', { users: result });
+      /*
+      const searchInput = document.querySelector('.input-users input');
+      const searchResultElement = document.querySelector('div.search-results');
+
+      const findUsers = (search) => {
+        return User.filter(user => {
+          return user.name.toLowerCase().includes(search);
+        });
+      }
+      const displayResults = (usersFound) => {
+        searchResultElement.innerHTML = '';
+
+        const usersList = document.createElement('ul');
+
+        usersFound.forEach(user => {
+          const userListElement = document.createElement('li');
+          userListElement.innerText = user.username;
+          usersList.appendChild(userListElement);
+        });
+        searchResultElement.appendChild(usersList);
+      };
+
+      const handleKeyUp = () => {
+        const search = searchInput.value.toLowerCase();
+        const usersFound = findUsers(search);
+        displayResults(usersFound);
+      };
+
+      searchInput.addEventListener('keyup', handleKeyUp);
     })
+  */ })
     .catch(next);
 });
 
